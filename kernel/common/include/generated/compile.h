e4c2450a187aef598b932fa3278cd96cfc8522f8
void-m41n <paul.austian.alday@gmail.com>
2013-09-26 01:03:05 +0800

    first commit

---
+++ kernel/common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Thu May 9 09:51:28 PHT 2013"
+#define LINUX_COMPILE_TIME "09:51:28"
+#define LINUX_COMPILE_BY "infuZion"
+#define LINUX_COMPILE_HOST "pxDaa-SYSTEMS"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
